5b3136e8bf4ec15cac473095bf59d38ae235e76c
geoftums <geoftums@gmail.com>
2019-10-03 10:48:41 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end("Hello world");
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
