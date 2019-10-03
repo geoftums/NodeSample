1a15eb654ae033a194c49d4414112f456457f256
geoftums <geoftums@gmail.com>
2019-10-03 11:39:23 +0400

    edited index js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req,res) =>{
   res.writeHead(200,{'Content-Type':'text/plain'});
-  res.end("Hello world");
+  res.end("Azure Testing...");
 });
 
 const port = process.env.PORT || 1337;
