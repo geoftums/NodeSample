506498b4ae13d66bbbc0c7e00f54355024ff1ad8
geoftums <geoftums@gmail.com>
2019-10-03 14:44:10 +0400

    edited index js

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ const server = http.createServer((req,res) =>{
 const port = process.env.PORT || 1337;
 server.listen(port);
 
+console.log("Testing...");
 console.log("Server running at http://localhost:%d", port);
 
